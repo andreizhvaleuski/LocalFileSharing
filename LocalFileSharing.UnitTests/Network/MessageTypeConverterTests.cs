c9b7a3f10b62a031111a1ebf5e1313417b13b7bd
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-22 15:12:48 +0300

    Tests for GetBytes method added.

---
+++ LocalFileSharing.UnitTests/Network/MessageTypeConverterTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+using LocalFileSharing.Network.Common;
+
+using NUnit.Framework;
+
+namespace LocalFileSharing.UnitTests.Network
+{
+    [TestFixture]
+    public class MessageTypeConverterTests
+    {
+        [TestCase(MessageType.Keepalive, MessageTypeConverter.Keepalive)]
+        [TestCase(MessageType.SendFileInitial, MessageTypeConverter.SendFileInitial)]
+        [TestCase(MessageType.SendFileRegular, MessageTypeConverter.SendFileRegular)]
+        [TestCase(MessageType.SendFileEnd, MessageTypeConverter.SendFileEnd)]
+        public void GetBytes_ValidMessageType_ReturnsValidBuffer(MessageType type, string typeShortName)
+        {
+            byte[] expectedTypeBuffer = Encoding.Unicode.GetBytes(typeShortName);
+
+            byte[] actualTypeBuffer = MessageTypeConverter.GetBytes(type);
+
+            Assert.AreEqual(actualTypeBuffer, expectedTypeBuffer);
+        }
+
+        [Test]
+        public void GetBytes_InvalidMessageType_ReturnsNullBuffer()
+        {
+            MessageType type = MessageType.Unspecified;
+
+            byte[] actualTypeBuffer = MessageTypeConverter.GetBytes(type);
+
+            Assert.IsNull(actualTypeBuffer);
+        }
+    }
+}
