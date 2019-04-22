0bc75ea21cc081519785263584045ce753d4ec0e
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-22 10:29:46 +0300

    Sockets added.

---
+++ LocalFileSharing.Network/Sockets/TcpClientSocket.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+using LocalFileSharing.Network.Content;
+using LocalFileSharing.Network.Framing.Wrappers;
+
+namespace LocalFileSharing.Network.Sockets
+{
+    public class TcpClientSocket
+    {
+        public const int MinPort = 61001;
+
+        public const int MaxPort = 65535;
+
+        protected readonly Socket client;
+
+        protected static Int32LengthPrefixWrapper lengthPrefixWrapper =
+            new Int32LengthPrefixWrapper();
+
+        public TcpClientSocket(IPEndPoint endPoint)
+        {
+            if (endPoint is null)
+            {
+                throw new ArgumentNullException(nameof(endPoint));
+            }
+
+            client = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            client.Connect(endPoint);
+        }
+
+        public void StartReceiveMessages()
+        {
+            while (true)
+            {
+                byte[] lengthBuffer = ReceiveBytes(4);
+                int length = BitConverter.ToInt32(lengthBuffer, 0);
+                byte[] buffer = ReceiveBytes(length);
+                DemoType type = (DemoType)BytesToObject(buffer);
+                Console.WriteLine($"FileName: {type.FileName}; BlockNumber: {type.BlocksNumber}");
+            }
+        }
+
+        protected int ReceiveMessageLength()
+        {
+            byte[] messageLengthBuffer = ReceiveBytes(4);
+            int messageLength = BitConverter.ToInt32(messageLengthBuffer, 0);
+            return messageLength;
+        }
+
+        protected string ReceiveMessageType()
+        {
+            byte[] messageTypeBuffer = ReceiveBytes(4);
+            string messageType = Encoding.Unicode.GetString(messageTypeBuffer);
+            return messageType;
+        }
+
+        protected byte[] ReceiveBytes(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            byte[] buffer = new byte[count];
+            int totalReceivedBytesNumber = 0;
+            do
+            {
+                int bytesReceivedNumber = client.Receive(
+                    buffer,
+                    totalReceivedBytesNumber,
+                    count - totalReceivedBytesNumber,
+                    SocketFlags.None
+                );
+                if (bytesReceivedNumber <= 0)
+                {
+                    return null;
+                }
+                totalReceivedBytesNumber += bytesReceivedNumber;
+            } while (totalReceivedBytesNumber < count);
+
+            return buffer;
+        }
+
+        protected object BytesToObject(byte[] buffer)
+        {
+            if (buffer is null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+
+            object deserializedObj = null;
+            using (MemoryStream stream = new MemoryStream(buffer))
+            {
+                deserializedObj = formatter.Deserialize(stream);
+            }
+
+            return deserializedObj;
+        }
+    }
+}
---
+++ LocalFileSharing.Network/Sockets/TcpServerSocket.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+
+using LocalFileSharing.Network.Content;
+
+namespace LocalFileSharing.Network.Sockets
+{
+    public class TcpServerSocket
+    {
+        public const int MinAllowedPort = 61001;
+
+        public const int MaxAllowedPort = 65535;
+
+        protected readonly Socket listener;
+
+        protected Socket connectedClient;
+
+        public TcpServerSocket(IPAddress localIPAddress, int port)
+        {
+            if (localIPAddress is null)
+            {
+                throw new ArgumentNullException(nameof(localIPAddress));
+            }
+
+            if (port >= MinAllowedPort && port <= MaxAllowedPort)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(port),
+                    port,
+                    $"Port number have to be between {MinAllowedPort} and {MaxAllowedPort}."
+                );
+            }
+
+            listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            listener.Bind(new IPEndPoint(localIPAddress, port));
+        }
+
+        public TcpServerSocket(IPEndPoint localEndPoint)
+        {
+            if (localEndPoint is null)
+            {
+                throw new ArgumentNullException(nameof(localEndPoint));
+            }
+
+            listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            listener.Bind(localEndPoint);
+        }
+
+        public void StartListening()
+        {
+            listener.Listen(1);
+
+            connectedClient = listener.Accept();
+        }
+
+        public void StopListening()
+        {
+            connectedClient?.Close();
+            listener.Close();
+        }
+
+        public void StartSendingMessages()
+        {
+            while (true)
+            {
+                Thread.Sleep(2000);
+                DemoType type = new DemoType()
+                {
+                    FileName = DateTime.Now.ToLongTimeString(),
+                    BlocksNumber = DateTime.Now.Ticks
+                };
+                SendObject(type);
+            }
+        }
+
+        public void SendObject(object obj)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+
+            byte[] buffer = null;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, obj);
+                buffer = stream.ToArray();
+            }
+            connectedClient.Send(MessageFraming.WrapMessage(buffer));
+        }
+    }
+}
