d273ed1c53d4c662bfe411066960cff8c0698b5e
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-22 19:34:22 +0300

    TypePrefixWrapper implemented.

--- LocalFileSharing.Network/Framing/Wrappers/TypePrefixWrapper.cs
+++ LocalFileSharing.Network/Framing/Wrappers/TypePrefixWrapper.cs
@@ -1,20 +1,54 @@
 ﻿using System;
 
+using LocalFileSharing.Network.Common;
+
 namespace LocalFileSharing.Network.Framing.Wrappers
 {
     public class TypePrefixWrapper : ITypePrefixWrapper
     {
-        public int TypePrefixBytes
+        public int TypePrefixSize => MessageTypeConverter.MessageTypeLength;
+
+        public byte[] Wrap(byte[] unwrappedBuffer, MessageType type)
         {
-            get
+            if (unwrappedBuffer is null)
             {
-                throw new NotImplementedException();
+                throw new ArgumentNullException(nameof(unwrappedBuffer));
             }
+
+            if (type == MessageType.Unspecified)
+            {
+                throw new ArgumentException(
+                    $"The argument {nameof(type)} cannot be with {MessageType.Unspecified} value.",
+                    nameof(type)
+                );
+            }
+
+            byte[] typePrefixBuffer = MessageTypeConverter.GetBytes(type);
+            byte[] wrappedBuffer = new byte[unwrappedBuffer.Length + typePrefixBuffer.Length];
+            typePrefixBuffer.CopyTo(wrappedBuffer, 0);
+            unwrappedBuffer.CopyTo(wrappedBuffer, typePrefixBuffer.Length);
+
+            return wrappedBuffer;
         }
 
-        public byte[] Wrap(byte[] source)
+        public MessageType GetTypePrefixValue(byte[] wrappedBuffer)
         {
-            throw new NotImplementedException();
+            if (wrappedBuffer is null)
+            {
+                throw new ArgumentNullException(nameof(wrappedBuffer));
+            }
+
+            if (wrappedBuffer.Length < TypePrefixSize)
+            {
+                throw new ArgumentException(
+                    $"The {nameof(wrappedBuffer)} length cannot be less than {TypePrefixSize} bytes.",
+                    nameof(wrappedBuffer)
+                );
+            }
+
+            MessageType type = MessageTypeConverter.GetMessageType(wrappedBuffer);
+
+            return type;
         }
     }
 }
