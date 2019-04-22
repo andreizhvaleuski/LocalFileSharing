533edc634daaefc5b076bdcb1c71111ccaa9d44c
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-22 16:26:30 +0300

    Tests for GetMessageType method added.

--- LocalFileSharing.UnitTests/Network/MessageTypeConverterTests.cs
+++ LocalFileSharing.UnitTests/Network/MessageTypeConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 using LocalFileSharing.Network.Common;
 
@@ -30,6 +31,39 @@ namespace LocalFileSharing.UnitTests.Network
             byte[] actualTypeBuffer = MessageTypeConverter.GetBytes(type);
 
             Assert.IsNull(actualTypeBuffer);
+        }
+
+        [TestCase(MessageTypeConverter.Keepalive, MessageType.Keepalive)]
+        [TestCase(MessageTypeConverter.SendFileInitial, MessageType.SendFileInitial)]
+        [TestCase(MessageTypeConverter.SendFileRegular, MessageType.SendFileRegular)]
+        [TestCase(MessageTypeConverter.SendFileEnd, MessageType.SendFileEnd)]
+        public void GetMessageType_ValidBuffer_ReturnsValidMessageType(string typeShortName, MessageType expectedType)
+        {
+            byte[] typeBuffer = Encoding.Unicode.GetBytes(typeShortName);
+
+            MessageType actualType = MessageTypeConverter.GetMessageType(typeBuffer);
+
+            Assert.AreEqual(expectedType, actualType);
+        }
+
+        [Test]
+        public void GetMessageType_NullBuffer_ThrowsArgumentNullException()
+        {
+            byte[] typeBuffer = null;
+
+            var ex = Assert.Throws<ArgumentNullException>(() => MessageTypeConverter.GetMessageType(typeBuffer));
+            Assert.That(ex.ParamName, Is.EqualTo("typeBuffer"));
+        }
+
+        [TestCase(0)]
+        [TestCase(MessageTypeConverter.MessageTypeLength - 1)]
+        [TestCase(MessageTypeConverter.MessageTypeLength + 1)]
+        public void GetMessageType_BufferWithInvalidLength_ThrowsArgumentException(int typeBufferLength)
+        {
+            byte[] typeBuffer = new byte[typeBufferLength];
+
+            var ex = Assert.Throws<ArgumentException>(() => MessageTypeConverter.GetMessageType(typeBuffer));
+            Assert.That(ex.ParamName, Is.EqualTo("typeBuffer"));
         }
     }
 }
