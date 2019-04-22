f3ddc1591e6336c8470b07a2694979d87d9016d5
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-22 10:30:36 +0300

    MessageType added.

---
+++ LocalFileSharing.Network/Common/MessageType.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalFileSharing.Network.Common
+{
+    public enum MessageType
+    {
+        Unknown = -1,
+        Unspecified = 0,
+        Keepalive = 1,
+        Utf16LittleEndian = 2,
+        Utf16BigEndian = 3
+    }
+}
