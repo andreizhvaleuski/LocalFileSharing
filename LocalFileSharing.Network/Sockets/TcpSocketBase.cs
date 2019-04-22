0a285701cc485374a9d44b88feace95057986337
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-22 11:28:00 +0300

    TcpSocketBase added and implemented.

---
+++ LocalFileSharing.Network/Sockets/TcpSocketBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LocalFileSharing.Network.Framing;
+
+namespace LocalFileSharing.Network.Sockets
+{
+    public abstract class TcpSocketBase
+    {
+        protected readonly ILengthPrefixWrapper lengthPrefixWrapper;
+
+        protected readonly ITypePrefixWrapper typePrefixWrapper;
+
+        public TcpSocketBase(
+            ILengthPrefixWrapper lengthPrefixWrapper,
+            ITypePrefixWrapper typePrefixWrapper
+        ) {
+            if (lengthPrefixWrapper is null)
+            {
+                throw new ArgumentNullException(nameof(lengthPrefixWrapper));
+            }
+
+            if (typePrefixWrapper is null)
+            {
+                throw new ArgumentNullException(nameof(typePrefixWrapper));
+            }
+
+            this.lengthPrefixWrapper = lengthPrefixWrapper;
+            this.typePrefixWrapper = typePrefixWrapper;
+        }
+    }
+}
