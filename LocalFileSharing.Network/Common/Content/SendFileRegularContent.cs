ff4816c8c0722520cf959a84ae98c92ff972297c
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-26 05:31:31 +0300

    SendFileRegularContent implemented.

--- LocalFileSharing.Network/Common/Content/SendFileRegularContent.cs
+++ LocalFileSharing.Network/Common/Content/SendFileRegularContent.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LocalFileSharing.Network.Common.Content
 {
-    public class SendFileRegularContent
+    public class SendFileRegularContent : FileBaseContent
     {
+        public SendFileRegularContent(Guid fileId, byte[] block)
+            : base(fileId)
+        {
+            if (block is null)
+            {
+                throw new ArgumentNullException(nameof(block));
+            }
+
+            if (block.Length <= 0)
+            {
+                throw new ArgumentException(
+                    $"The block length must be greater than 0.",
+                    nameof(block)
+                );
+            }
+
+            Block = block;
+        }
+
+        public IReadOnlyCollection<byte> Block { get; private set; }
     }
 }
