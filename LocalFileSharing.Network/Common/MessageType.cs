9fd0dc75927ee6468430d516f4e021c4097dd77a
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-26 05:41:25 +0300

    Message types added.

--- LocalFileSharing.Network/Common/MessageType.cs
+++ LocalFileSharing.Network/Common/MessageType.cs
@@ -5,7 +5,12 @@
         Unspecified = 0,
         Keepalive = 1,
         SendFileInitial = 2,
-        SendFileRegular = 3 ,
-        SendFileEnd = 4
+        SendFileRegular = 3,
+        SendFileEnd = 4,
+        SendFileCancel = 5,
+        ReceiveFileInitialize = 6,
+        ReciveFileRegular = 7,
+        ReceiveFileEnd = 8,
+        ReceiveFileCancel = 9
     }
 }
