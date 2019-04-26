84e90d938aac438508122147562925ea724951a3
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-26 05:58:23 +0300

    SendFileInitContent renamed to SendFileInitialContent.

---
+++ LocalFileSharing.Network/Common/Content/SendFileInitialContent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LocalFileSharing.Network.Common.Content
+{
+    public class SendFileInitialContent : FileBaseContent
+    {
+        public SendFileInitialContent(Guid fileId, string fileName, long fileSize, byte[] sha256FileHash)
+            : base(fileId)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException(
+                    $"The file name can not be null or whitespace.",
+                    nameof(fileName)
+                );
+            }
+
+            if (fileSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    $"The file size must be greater than 0 bytes.",
+                    nameof(fileSize)
+                );
+            }
+
+            if (sha256FileHash is null)
+            {
+                throw new ArgumentNullException(nameof(sha256FileHash));
+            }
+
+            if (sha256FileHash.Length <= 0)
+            {
+                throw new ArgumentException(
+                    $"The file hash length must be greater than 0.",
+                    nameof(sha256FileHash)
+                );
+            }
+
+            FileName = fileName;
+            FileSize = fileSize;
+            Sha256FileHash = sha256FileHash;
+        }
+
+        public string FileName { get; private set; }
+
+        public long FileSize { get; private set; }
+
+        public IEnumerable<byte> Sha256FileHash { get; private set; }
+    }
+}
