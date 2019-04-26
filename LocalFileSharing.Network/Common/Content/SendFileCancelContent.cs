333a28a8c559484bb72ca54ca2c63cd19ea1c7a4
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-26 05:56:00 +0300

    SendFileCancelContent added.

---
+++ LocalFileSharing.Network/Common/Content/SendFileCancelContent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocalFileSharing.Network.Common.Content
+{
+    public class SendFileCancelContent : FileBaseContent
+    {
+        public SendFileCancelContent(Guid fileId)
+            : base(fileId)
+        {
+        }
+    }
+}
