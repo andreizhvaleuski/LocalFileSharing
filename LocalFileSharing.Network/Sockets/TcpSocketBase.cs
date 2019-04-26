f0f21bf0cadae5d14e5179521fcfa2699831b25e
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-04-26 09:02:44 +0300

    TcpSocketBase reworked.

--- LocalFileSharing.Network/Sockets/TcpSocketBase.cs
+++ LocalFileSharing.Network/Sockets/TcpSocketBase.cs
@@ -1,30 +1,8 @@
-﻿using System;
-using LocalFileSharing.Network.Framing;
-
-namespace LocalFileSharing.Network.Sockets
+﻿namespace LocalFileSharing.Network.Sockets
 {
     public abstract class TcpSocketBase
     {
-        protected readonly ILengthPrefixWrapper lengthPrefixWrapper;
-
-        protected readonly ITypePrefixWrapper typePrefixWrapper;
-
-        public TcpSocketBase(
-            ILengthPrefixWrapper lengthPrefixWrapper,
-            ITypePrefixWrapper typePrefixWrapper
-        ) {
-            if (lengthPrefixWrapper is null)
-            {
-                throw new ArgumentNullException(nameof(lengthPrefixWrapper));
-            }
-
-            if (typePrefixWrapper is null)
-            {
-                throw new ArgumentNullException(nameof(typePrefixWrapper));
-            }
-
-            this.lengthPrefixWrapper = lengthPrefixWrapper;
-            this.typePrefixWrapper = typePrefixWrapper;
-        }
+        public const int MinAllowedPort = 61001;
+        public const int MaxAllowedPort = 65535;
     }
 }
