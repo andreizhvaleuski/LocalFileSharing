0626c2eb4842c085ae7e866f22a56150489e61a6
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-01 12:27:50 +0300

    FileData reworked.

---
+++ LocalFileSharing.Domain/Infrastructure/FileData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LocalFileSharing.Domain.Infrastructure
+{
+    public class FileData
+    {
+        public Guid FileId { get; private set; }
+
+        public string FilePath { get; private set; }
+
+        public long FileSize { get; private set; }
+
+        public FileData(Guid fileId, string filePath, long fileSize)
+        {
+            if (fileId == Guid.Empty)
+            {
+                throw new ArgumentException(nameof(fileId));
+            }
+
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentException(nameof(filePath));
+            }
+
+            if (fileSize <= 0)
+            {
+                throw new ArgumentNullException(nameof(fileSize));
+            }
+
+            FileId = fileId;
+            FilePath = filePath;
+            FileSize = fileSize;
+        }
+    }
+}
--- LocalFileSharing.Domain/Infrastructure/SendFileProgressReport.cs
+++ LocalFileSharing.Domain/Infrastructure/SendFileProgressReport.cs
@@ -1,25 +1,11 @@
-﻿using LocalFileSharing.Network.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LocalFileSharing.Domain.Infrastructure
+﻿namespace LocalFileSharing.Domain.Infrastructure
 {
     public class SendFileProgressReport
     {
         public SendFileState SendFileState { get; private set; }
 
-        public string FilePath { get; private set; }
+        public FileData FileData { get; private set; }
 
-        public long FileSize { get; private set; }
-
-        public long SentSize { get; private set; }
-
-        public SendFileProgressReport(SendFileState sendFileState)
-        {
-            SendFileState = sendFileState;
-        }
+        public long BytesSent { get; set; }
     }
 }
