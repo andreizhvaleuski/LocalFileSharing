f032ea00b377e2de398f8d8cea73db087425d427
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-01 12:03:14 +0300

    SendFileProgressReport class added.

---
+++ LocalFileSharing.Domain/Infrastructure/SendFileProgressReport.cs
@@ -0,0 +1,25 @@
+﻿using LocalFileSharing.Network.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalFileSharing.Domain.Infrastructure
+{
+    public class SendFileProgressReport
+    {
+        public SendFileState SendFileState { get; private set; }
+
+        public string FilePath { get; private set; }
+
+        public long FileSize { get; private set; }
+
+        public long SentSize { get; private set; }
+
+        public SendFileProgressReport(SendFileState sendFileState)
+        {
+            SendFileState = sendFileState;
+        }
+    }
+}
