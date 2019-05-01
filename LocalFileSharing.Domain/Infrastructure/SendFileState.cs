4cdfe9da19ca144c04dc075c2c88ca2da25ef058
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-01 12:02:52 +0300

    SendFileState enum added.

---
+++ LocalFileSharing.Domain/Infrastructure/SendFileState.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalFileSharing.Domain.Infrastructure
+{
+    public enum SendFileState
+    {
+        Unspecified = 0,
+        Hashing = 1,
+        Initializing = 2,
+        Sending = 3,
+        Ending = 4
+    }
+}
