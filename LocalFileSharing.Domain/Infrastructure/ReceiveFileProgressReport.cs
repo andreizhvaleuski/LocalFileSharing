eab72ed0b5c2bf6c473fa04aad1a88a347510254
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-07 15:28:20 +0300

    .

--- LocalFileSharing.Domain/Infrastructure/ReceiveFileProgressReport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LocalFileSharing.Domain.Infrastructure {
-    public class ReceiveFileProgressReport {
-        public ReceiveFileState ReceiveFileState { get; set; }
-
-        public FileData FileData { get; set; }
-
-        public long BytesRecived { get; set; }
-    }
-}
--- LocalFileSharing.Domain/Infrastructure/SendFileProgressReport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LocalFileSharing.Domain.Infrastructure {
-    public class SendFileProgressReport {
-        public SendFileState SendFileState { get; set; }
-
-        public FileData FileData { get; set; }
-
-        public long BytesSent { get; set; }
-    }
-}
