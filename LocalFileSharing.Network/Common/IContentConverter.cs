c2b8d3e36d5d86b1850425f5490797bf70588474
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-07 11:59:56 +0300

    IContentConverter added.

---
+++ LocalFileSharing.Network/Common/IContentConverter.cs
@@ -0,0 +1,9 @@
+﻿using LocalFileSharing.Network.Common.Content;
+
+namespace LocalFileSharing.Network.Common {
+    public interface IContentConverter {
+        byte[] GetBytes(ContentBase content);
+
+        ContentBase GetContent(byte[] contentBuffer);
+    }
+}
