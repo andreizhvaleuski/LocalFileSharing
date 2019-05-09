248c0ef1d35acdc1e826b048d1b68574eaed5e26
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-09 09:02:10 +0300

    FileTransferContextBase changed.

--- LocalFileSharing.Network/Domain/Context/FileTransferContextBase.cs
+++ LocalFileSharing.Network/Domain/Context/FileTransferContextBase.cs
@@ -1,48 +1,12 @@
 ﻿using System;
 
 namespace LocalFileSharing.Network.Domain.Context {
-    public abstract class FileTransferContextBase
-        : IComparable<FileTransferContextBase>, IEquatable<FileTransferContextBase> {
+    public abstract class FileTransferContextBase {
         private bool _isCancelled;
         private bool _isFailed;
 
-        public Guid TransferID { get; protected set; }
         public bool Cancelled => _isCancelled;
         public bool Failed => _isFailed;
-
-        public FileTransferContextBase(Guid transferID) {
-            if (transferID == Guid.Empty) {
-                throw new ArgumentException(
-                    $"The transfer id can not be empty.",
-                    nameof(transferID)
-                );
-            }
-        }
-
-        public int CompareTo(FileTransferContextBase other) {
-            if (other is null) {
-                return 1;
-            }
-            return TransferID.CompareTo(other.TransferID);
-        }
-
-        public bool Equals(FileTransferContextBase other) {
-            if (other is null) {
-                return false;
-            }
-            return TransferID.Equals(other.TransferID);
-        }
-
-        public override bool Equals(object obj) {
-            if (obj != null && obj is SendFileContext context) {
-                return Equals(context);
-            }
-            return false;
-        }
-
-        public override int GetHashCode() {
-            return TransferID.GetHashCode();
-        }
 
         public abstract void Close();
 
@@ -50,7 +14,6 @@ namespace LocalFileSharing.Network.Domain.Context {
             if (Cancelled) {
                 throw new InvalidOperationException();
             }
-
             _isCancelled = true;
         }
     }
