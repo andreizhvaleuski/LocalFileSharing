b693ebcd147dae19a27a2aef61a2154f19853aa8
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-10 15:51:36 +0300

    End method overridden.

--- LocalFileSharing.Network/Domain/Context/SendFileContext.cs
+++ LocalFileSharing.Network/Domain/Context/SendFileContext.cs
@@ -5,6 +5,10 @@ namespace LocalFileSharing.Network.Domain.Context {
         public long BytesSent { get; set; }
         public BinaryReader Reader { get; set; }
 
+        public override void End() {
+            Reader.Close();
+        }
+
         public override void Cancel() {
             base.Cancel();
             Reader.Close();
