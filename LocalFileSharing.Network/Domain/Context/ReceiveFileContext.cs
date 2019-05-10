25dd5f3b2ea70e18ddf46d5d87cf04b04914bee6
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-10 15:32:07 +0300

    Cancel method overridden.

--- LocalFileSharing.Network/Domain/Context/ReceiveFileContext.cs
+++ LocalFileSharing.Network/Domain/Context/ReceiveFileContext.cs
@@ -4,5 +4,10 @@ namespace LocalFileSharing.Network.Domain.Context {
     public class ReceiveFileContext : FileTransferContextBase {
         public long BytesReceived { get; set; }
         public BinaryWriter Writer { get; set; }
+
+        public override void Cancel() {
+            base.Cancel();
+            Writer.Close();
+        }
     }
 }
