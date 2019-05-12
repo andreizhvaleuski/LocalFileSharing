ad5d394514187a728c7c246b8e787317e1302fa2
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2019-05-12 07:30:35 +0300

    Dialogs implemented.

---
+++ LocalFileSharing.DesktopUI/Services/DialogService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace LocalFileSharing.DesktopUI.Services {
+    public class DialogService : IDialogService {
+        public string[] OpenFilePaths { get; private set; }
+        public string SaveFilePath { get; private set; }
+        public bool OpenFileDialog() {
+            OpenFileDialog openFileDialog = new OpenFileDialog() {
+                Multiselect = true,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            if (openFileDialog.ShowDialog() == true) {
+                OpenFilePaths = openFileDialog.FileNames;
+                return true;
+            }
+            return false;
+        }
+
+        public bool SaveFileDialog(string fileName) {
+            if (string.IsNullOrWhiteSpace(fileName)) {
+                return false;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog {
+                FileName = fileName,
+                DefaultExt = Path.GetExtension(fileName),
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            if (saveFileDialog.ShowDialog() == true) {
+                SaveFilePath = saveFileDialog.FileName;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ LocalFileSharing.DesktopUI/Services/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalFileSharing.DesktopUI.Services {
+    public interface IDialogService {
+        string[] OpenFilePaths { get; }
+        string SaveFilePath { get; }
+        bool OpenFileDialog();
+        bool SaveFileDialog(string fileName);
+    }
+}
